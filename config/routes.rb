eb99f25a2677f5e7c1fe86e286d532b7f4ec27cc
harshit <harshitagarwalsmt@gmail.com>
2023-08-21 02:25:01 +0530

    authentication in rails

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root 'home#homepage'
+  get 'homepage', to:'home#homepage'
+  get 'dashboard', to:'home#dashboard'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
